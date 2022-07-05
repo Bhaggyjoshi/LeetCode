cec21348ca785a9f1f85b3a33337cfa3fb0f0cb9
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-07-05 12:20:22 +0530

    Adding new java program to sort array and square the array

---
+++ Fourteen_Days/SquareofSortedArray.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SquareofSortedArray {
+
+    // public static void squareSortArray(int[] arr) {
+    //    for (int ele : arr) {
+    //     ele*=ele;
+        
+
+    //     System.out.println(ele);
+    //    }
+       
+    // }
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k;
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+        k= scn.nextInt();
+        arr[i]=k*k;
+           
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+          System.out.println(arr[i]);  
+        }
+
+        // squareSortArray(arr);
+       
+    }
+
+   
+}
